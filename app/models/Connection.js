ff994d0a6fc344e07302383f9d144ebce2cea879
Gabriel Cebrian <gabceb@gmail.com>
2018-10-04 11:43:07 -0400

    Added example on why rematch looks promising

--- app/models/Connection.js
+++ app/models/Connection.js
@@ -15,3 +15,39 @@ export default () => ({
     },
   }),
 });
+
+/*
+
+*********** What the same code looks like with redux duck pattern + sagas *************
+
+const actionsPrefix = 'connection';
+const SET_IS_CONNECTED = `${actionsPrefix}/SET_IS_CONNECTED`;
+const LOAD_IS_CONNECTED = `${actionsPrefix}/LOAD_IS_CONNECTED`;
+export const defaultState = Object.freeze({
+  isConnected: true,
+});
+export default function reducer(state = defaultState, action) {
+  const { type, data } = action;
+  switch (type) {
+    case SET_IS_CONNECTED:
+      return { ...state, isConnected: data.isConnected };
+    default:
+      return state;
+  }
+}
+export const setIsConnected = (isConnected) => ({ type: SET_IS_CONNECTED, data: { isConnected } });
+export const loadIsConnected = () => ({ type: LOAD_IS_CONNECTED });
+export function* loadIsConnectedSaga() {
+  const connectionStatus = yield call(NetInfo.isConnected.fetch);
+  yield put(setIsConnected(connectionStatus));
+}
+export function sagas() {
+  return {
+    loadIsConnected: function* loadIsConnectedGen() { yield takeLatest(LOAD_IS_CONNECTED, loadIsConnectedSaga); },
+  };
+}
+// Selectors
+const get = (state) => state.connection;
+export const isConnectedSelector = createSelector(get, ({ isConnected }) => isConnected);
+
+*/
