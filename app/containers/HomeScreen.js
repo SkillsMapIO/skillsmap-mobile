3777983992a1b7b04f39206bbbe02f002df5cb87
Gabriel Cebrian <gabceb@gmail.com>
2018-10-04 00:04:37 -0400

    Added a logout button to the home screen

--- app/containers/HomeScreen.js
+++ app/containers/HomeScreen.js
@@ -6,12 +6,18 @@ import {
   Text,
   View,
 } from 'react-native';
+import { Button } from 'react-native-elements';
 import connect from 'react-redux/es/connect/connect';
 
 // Styles
 import Styles from './styles/HomeScreenStyles';
+import { systemColors } from '../themes/Colors';
 
 class HomeScreen extends React.Component {
+  onLogoutPress = () => {
+    this.props.logout();
+  }
+
   render() {
     const welcomeMessage = 'Welcome';
 
@@ -21,6 +27,12 @@ class HomeScreen extends React.Component {
         <View style={Styles.header}>
           <Text style={Styles.headerWelcomeMessage} numberOfLines={1}>{welcomeMessage}</Text>
         </View>
+        <Button
+          onPress={this.onLogoutPress}
+          backgroundColor={systemColors.primaryButton}
+          title="Logout"
+          raised
+        />
       </View>
     );
   }
@@ -32,6 +44,7 @@ HomeScreen.propTypes = {
     dispatch: PropTypes.func.isRequired,
     state: PropTypes.object.isRequired,
   }).isRequired,
+  logout: PropTypes.func.isRequired,
 };
 const mapStateToProps = () => ({});
 
