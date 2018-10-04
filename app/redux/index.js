3760688e18dd41177a438ff4d7020389815d3b20
Gabriel Cebrian <gabceb@gmail.com>
2018-10-03 23:23:24 -0400

    Added redux logger

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,4 +1,5 @@
 import { init } from '@rematch/core';
+import { createLogger } from 'redux-logger';
 
 import Models from '../models';
 import config from '../config/DebugConfig';
@@ -8,9 +9,37 @@ import FixtureApi from '../services/FixtureApi';
 
 export default () => {
   const api = config.useFixtures ? FixtureApi() : Api(ApiConfig);
+  const middlewares = [];
+
+  /* ------------- Logger Middleware ------------- */
+
+  // remove common noise
+  const loggingBlacklist = ['EFFECT_TRIGGERED', 'EFFECT_RESOLVED', 'EFFECT_REJECTED'];
+  if (__DEV__) {
+    // silence these saga-based messages
+    // create the logger
+    const logger = createLogger({
+      predicate: (getState, { type }) => {
+        const isBlacklisted = loggingBlacklist.includes(type);
+
+        if (isBlacklisted) {
+          return false;
+        }
+
+        return true;
+      },
+    });
+
+    middlewares.push(logger);
+  }
+
+  /* ------------- Init rematch ------------- */
   const store = init({
     models: Models(api),
     plugins: [],
+    redux: {
+      middlewares,
+    },
   });
 
   return store;
